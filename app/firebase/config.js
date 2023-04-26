d68b5edd70047c6cc9ef31c2c58d96e0c36aac7e
Vishavjeet Singh <vishavbehar@gmail.com>
2023-04-26 14:38:19 +0530

    Did some changes to firebase config file

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,20 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import Constants from "expo-constants";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(Constants.manifest.extra);
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.firebaseApiKey,
-  authDomain: Constants.manifest.extra.firebaseAuthDomain,
-  projectId: Constants.manifest.extra.firebaseProjectId,
-  storageBucket: Constants.manifest.extra.firebaseStorageBucket,
-  messagingSenderId: Constants.manifest.extra.firebaseMessagingSenderId,
-  appId: Constants.manifest.extra.firebaseAppId,
-  measurementId: Constants.manifest.extra.firebaseMeasurementId
+  apiKey: "AIzaSyCJs5IwTVGlb-y15Q19hTvjq5XCdVZ-iqs",
+  authDomain: "credit-io.firebaseapp.com",
+  projectId: "credit-io",
+  storageBucket: "credit-io.appspot.com",
+  messagingSenderId: "3354402696",
+  appId: "1:3354402696:web:dc797cda2a6f5c60e8643d",
+  measurementId: "G-0EMZDJ2DMY"
 };
 
 // Initialize Firebase
